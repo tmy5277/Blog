b9b9e33acfd00d958f9b62cfc379a267fc55e186
tmy5277 <1223688937@qq.com>
2017-09-14 16:15:48 +0800

    fix bug

--- done/waterfall-jQuery/js/waterfall.js
+++ done/waterfall-jQuery/js/waterfall.js
@@ -1,6 +1,6 @@
 $( window ).on( "load", function(){
     waterfall('main','pin');
-    var dataInt = {"data":[{"src":'26.jpg'},{"src":'27.jpg'},{"src":'28.jpg'},{"src":'29.jpg'},{"src":'30.jpg'},{"src":'31.jpg'},{"src":'32.jpg'},{"src":'33.jpg'}]}
+    var dataInt = {"data":[{"src":'31.jpg'},{"src":'32.jpg'},{"src":'33.jpg'},{"src":'34.jpg'},{"src":'35.jpg'},{"src":'36.jpg'},{"src":'37.jpg'},{"src":'38.jpg'}]}
     window.onscroll=function(){
         if(checkscrollside()){
             $.each( dataInt.data, function( index, value ){
@@ -21,7 +21,6 @@ function waterfall(parent,pin){
     var $aPin = $( "#main>div" );
     var iPinW = $aPin.eq( 0 ).width();// 一个块框pin的宽
     var num = Math.floor( $( window ).width() / iPinW );//每行中能容纳的pin个数【窗口宽度除以一个块框宽度】
-    
     //设置父级居中样式：定宽+自动水平外边距
     $( "#main" ).css({
         'width:' : iPinW * num,
@@ -37,13 +36,15 @@ function waterfall(parent,pin){
         }else{
             var minH = Math.min.apply( null, pinHArr );//数组pinHArr中的最小值minH
             var minHIndex = $.inArray( minH, pinHArr );
+            
             $( value ).css({
                 'position': 'absolute',
-                'top': minH + 15,
+                'top': minH + 14,
                 'left': $aPin.eq( minHIndex ).position().left
             });
             //数组 最小高元素的高 + 添加上的aPin[i]块框高
-            pinHArr[ minHIndex ] += $aPin.eq( index ).height()+15;//更新添加了块框后的列高
+            pinHArr[ minHIndex ] += $aPin.eq( index ).height()+14;//更新添加了块框后的列高
+           
         }
     });
 }
